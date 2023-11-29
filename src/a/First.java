0ad403778de321d4795aeb40379d94172a2803d0
hongseokone <wjsrl20033@gmail.com>
2023-11-29 15:39:53 +0900

    first commit

---
+++ src/a/First.java
@@ -0,0 +1,18 @@
+// 현재 클래스(자바파일)가 소속된 패키지의 이름
+package a;
+
+// 현재 클래스의 이름
+public class First { // (First class 블록의시작)파일이름에따라 (public class 이름)
+	
+	// main 메서드(프로그램의 시작 지점)
+	public static void main(String[] args) { // main 메서드 블록의 시작
+		// 실행할 내용이 작성되는 공간 // 검은색은 바꿀수있는 구문 / 보라색 : 바꿀수없는 구문
+		// 중가로를 잘볼필요가 있음 (어디서 열려서 어디서 닫히는지)
+		// System.out.println("자바야 앞으로 친하게 지내자.."); 동작을 받고 ;으로 끝냄
+		//
+		
+		// 텍스트 또는 변수값을 출력할 수 있는 println() 메서드 
+		System.out.println("자바야 앞으로 친하게 지내자..");		
+	} // main 메서드 블록의 끝
+
+} // First class 블록의 끝를 
